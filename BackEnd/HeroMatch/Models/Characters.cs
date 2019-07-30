1d51c156ec95e7c0ae111ab6f4a105ea6285cd07
Sergio Rodriguez <rsergio98@yahoo.com>
2019-07-30 15:08:48 -0400

    Created models, not all have properties yet

---
+++ BackEnd/HeroMatch/Models/Characters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeroMatch.Models
+{
+    public class Characters
+    {
+        public int CharacterId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Difficulty { get; set; }
+        public string Api { get; set; }
+        public string Video { get; set; }
+    }
+}

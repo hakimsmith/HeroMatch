1208b26f6d4aef9f62a2b6855359cce717d1237b
Blair <bgriffit@oberlin.edu>
2019-07-31 15:47:34 -0400

    started working on character quiz logic in app.js

--- Parcel-frontend/js/app.js
+++ Parcel-frontend/js/app.js
@@ -1,14 +1,17 @@
 import Home from './components/home';
 import GameQuiz from './components/gameQuiz';
-import CharacterQuiz from './components/characterQuiz'
+import CharacterQuiz from './components/characterQuiz';
 
+console.log("app.js");
 
 pageBuild();
 
 function pageBuild(){
+    console.log("page build");
     home();
+    characterQuiz();
     
-}
+};
 
 function home(){
     const body = document.getElementById('root');
@@ -16,5 +19,23 @@ function home(){
     home.addEventListener('click', function(){
         body.innerHTML = Home();
     });
+};
+
+function characterQuiz(){
+    let difficulty = 0;
+    let role = 0;
+    let subrole = 0;
+
+    document.getElementById('quizbutton').addEventListener('click', function(){
+        document.getElementById('quiz').innerHTML = CharacterQuiz();
+    })
+
+    document.querySelector('#quiz').addEventListener('click', function(){
+        if(event.target.classList.contains('option')){
+            difficulty = event.target.value
+            console.log(difficulty)
+        }
+    })
+
 };
 

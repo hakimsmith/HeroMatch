e5d2de0b623f52cec4c6123f22c9a8d4a714f1a7
mega24cde <megan.denman@gmail.com>
2019-07-31 13:07:53 -0400

    continued refining one question for quiz

--- Parcel-frontend/js/components/character-quiz.js
+++ Parcel-frontend/js/components/character-quiz.js
@@ -1,3 +1,10 @@
+const qu1scoreKeeper = {
+  tank: A,
+  support: B,
+  damage: C,
+  
+};
+
 export default function CharQuiz(charquiz){
 return`
 
@@ -7,18 +14,11 @@ return`
 <button id="submit">Submit Quiz</button>
 <div id="results"></div>
 
-const myQuestions = [
-    {
-      question: "Who is the strongest?",
-      answers: {
-        a: "Superman",
-        b: "The Terminator",
-        c: "Waluigi, obviously"
-      },
-      correctAnswer: "c"
-    },
-    
-    
-  ];
+const questions = [
+  {
+    prompt: "Which of these roles would be most appealing to you?\nA.) A lead role, where I decide the direction the team is headed. \nB.) A supportive role, where I help others perform their roles optimally. \nC.) A damage role, where I focus on taking down a specific enemy."
+    answer: ${userAnswer}
+  }
+]
 `
 }

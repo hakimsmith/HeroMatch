577e26bf6a9dc4603b313894c1425e9fcbc02c32
Sergio Rodriguez <rsergio98@yahoo.com>
2019-08-02 13:29:48 -0400

    added OW and fixed the picture URL's

---
+++ BackEnd/HeroMatch/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeroMatch.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Download {get; set;}
+        public string Video { get; set; }
+
+        public virtual List<Character> Character { get; set; }
+
+    }
+}

99fe0821695bc9e044cf4d37d5943ad8c5655d87
Hakim Smith <hakim.smith19@yahoo.com>
2019-08-05 14:02:50 -0400

    worked on aesthetics component for league of legends

--- Parcel-frontend/js/components/home.js
+++ Parcel-frontend/js/components/home.js
@@ -1,5 +1,19 @@
-export default function Home(){
+export default function Home(games){
     return `
-        Hello World!!!!!!!!!
+    <h1>Pick a Game</h1>
+    <ul class='main-flex'>
+        ${games.map(game => {
+        return `
+            <li>
+                <img class='select-game' src="${game.image}">
+                <p>${game.name}</p>
+                <input class='select-game__id' type='hidden' value="${game.gameId}">
+            
+            </li>
+            `;
+        })
+        .join("")}
+    </ul>
+        
     `
 }

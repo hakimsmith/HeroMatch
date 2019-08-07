cb5ffa8c950eac2eae8afe5ab665b1402ca9e3d3
mega24cde <megan.denman@gmail.com>
2019-08-06 20:08:59 -0400

    changed css to center elements , including submit button and game titles and images. Also changed some text color.

--- Parcel-frontend/js/components/aesthetic-options.js
+++ Parcel-frontend/js/components/aesthetic-options.js
@@ -4,9 +4,9 @@ export default function Aesthetic(){
         <h2>What form would you like your character to be?</h2>
     </div>
     <div class=Question_Choices value='1'>
-        <button class='aestheticOption' value='1'>I choose to be a human.</button>
-        <button class='aestheticOption' value='2'>I choose to be a humanoid, half human, half something else.</button>
-        <button class='aestheticOption' value='3'>I choose to be an animal.</button>
+        <button class='quiz-button aestheticOption' value='1'>I choose to be a human.</button>
+        <button class='quiz-button aestheticOption' value='2'>I choose to be a humanoid, half human, half something else.</button>
+        <button class='quiz-button aestheticOption' value='3'>I choose to be an animal.</button>
     </div>
     `
 }

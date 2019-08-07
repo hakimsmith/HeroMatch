1d4c9e8d691b4f9a52e3b2b4de0df0e9ed11b52c
Sergio Rodriguez <rsergio98@yahoo.com>
2019-08-07 14:42:26 -0400

    character image naming fix

---
+++ BackEnd/HeroMatch/Migrations/20190807174518_FixexCapatalization.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HeroMatch.Migrations
+{
+    public partial class FixexCapatalization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 45,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/LeeSin/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/LeeSin_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/LeeSin.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 46,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/MasterYi/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/MasterYi_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/MasterYi.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 61,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/DrMundo/", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/DrMundo.png", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/DrMundo.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 93,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/MissFortune/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/MissFortune_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/MissFortune.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 123,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/AurelionSol/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/AurelionSol_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/AurelionSol.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 145,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/TwistedFate/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/TwistedFate_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/TwistedFate.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 162,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/TahmKench/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/TahmKench_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/TahmKench.png" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 45,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/Leesin/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Leesin_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Leesin.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 46,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/Masteryi/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Masteryi_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Masteryi.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 61,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/Drmundo/", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Drmundo.png", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Drmundo.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 93,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/Missfortune/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Missfortune_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Missfortune.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 123,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/Aurelionsol/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aurelionsol_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Aurelionsol.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 145,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/Twistedfate/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Twistedfate_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Twistedfate.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 162,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/Tahmkench/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Tahmkench_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Tahmkench.png" });
+        }
+    }
+}
---
+++ BackEnd/HeroMatch/Migrations/20190807175405_charupdates.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HeroMatch.Migrations
+{
+    public partial class charupdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 50,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/RekSai/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/RekSai_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/RekSai.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 91,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/KogMaw/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/KogMaw_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/KogMaw.png" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 50,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/Reksai/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Reksai_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Reksai.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Character",
+                keyColumn: "CharacterId",
+                keyValue: 91,
+                columns: new[] { "CharacterUrl", "Image", "SmallImage" },
+                values: new object[] { "https://na.leagueoflegends.com/en/game-info/champions/Kogmaw/", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Kogmaw_0.jpg", "https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/Kogmaw.png" });
+        }
+    }
+}

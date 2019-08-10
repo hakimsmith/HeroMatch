7465814f03fcb3cb31f9a89667c3fd45b7008447
mega24cde <megan.denman@gmail.com>
2019-08-10 17:26:22 -0400

    better fonts, and shadow styling, cleaned up duplicate css code

---
+++ Parcel-frontend/js/components/about.js
@@ -0,0 +1,29 @@
+export default function About(){
+    return `
+    <header>
+    <h1>Hero Match</h1>
+   
+
+        <span>Find Your Gaming Character</span>   
+   
+</header>
+<div class = "about">
+    <h2>About</h2>
+
+        <p>This website helps novice gamers navigate the process of finding a hero character to utilize in a game. You don't need to know anything about gaming or any particular game's terminologies in order to be served by this program. Just answer the questions, and we will take it from there! When you take the quiz, we first determine which game you'd like to play. Once we have your game choice, then we narrow down your character choices based on input from you. You can choose the #1 recommended hero, or choose one of the other available options. Then, play your game, and have fun!</p>
+
+    <h2>Developers:</h2>
+        <p> Angel Davis</p>
+        </br>
+        <p> Megan Denman </p>
+        </br>     
+        <p> Aaron Ferguson </p>
+        </br>     
+        <p> Blair Griffith </p>
+        </br>     
+        <p> Serio Rodriguez</p>
+        </br>     
+        <p> Hakim Smith </p>
+    </div>    
+    `
+}
